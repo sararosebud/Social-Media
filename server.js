7d09828b68e9ac856c5a21a51871c35f25b35876
Sara Wendland <info@rosebudsara.com>
2023-06-17 12:36:11 -0500

    initialized server

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const db = require('./config/connection');
+// const routes = require('./routes');
 
-const cwd = process.cwd();
+// const cwd = process.cwd();
+
 const PORT = 3001;
 const app = express();
 
-const activity = cwd.includes('18Challenge')
-  ? cwd.split('/18Challenge/')[1]
-  : cwd;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+// set up home route
+app.get('/', (request, respond) => {
+  respond.status(200).json({
+    message: 'Welcome to Puddle Playgroup. An app for parntes looking for incliment weather ideas and meetups',
   });
 });
+app.listen(PORT, (request, respond) => {
+  console.log(`Our server is live on ${PORT}. Yay!`);
+});
+
+
+// const activity = cwd.includes('18Challenge')
+//   ? cwd.split('/18Challenge/')[1]
+//   : cwd;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server for ${activity} running on port ${PORT}!`);
+//   });
+// });
